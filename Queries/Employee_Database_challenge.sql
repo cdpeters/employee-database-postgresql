68d9db9c50ad403b48869b386897478f92a3ef94
Chris Peterson <cdpeters1@gmail.com>
2022-02-12 05:20:25 -0800

    Fix deliverable 3 question 2 query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -49,7 +49,7 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
 
 -- Deliverable 3.
 
--- Question 1: How many roles will need to be filled if the employees that meet
+-- QUESTION 1: How many roles will need to be filled if the employees that meet
 -- retirement eligibility choose to retire?
 
 -- First create a table with retirement eligible employees with their titles and
@@ -78,7 +78,8 @@ SELECT rtd.dept_name,
  GROUP BY rtd.dept_name
  ORDER BY ct DESC;
 
--- Question 2: Are there enough qualified, retirement-ready employees to mentor
+--------------------------------------------------------------------------------
+-- QUESTION 2: Are there enough qualified, retirement-ready employees to mentor
 -- new employees?
 
 -- Assumption: New employees will refer to the most recent year where there is
@@ -92,41 +93,32 @@ SELECT *
 -- 1999 is the most recent year with complete hiring data. New employees will
 -- now refer to employees hired in 1999.
 
-
--- First create a table of all employees listed with their titles and hire dates
+-- First create a table of all employees listed with their current title and
+-- hire date
 SELECT DISTINCT ON(e.emp_no) e.emp_no,
 	   e.first_name, e.last_name,
-	   EXTRACT('year' FROM e.hire_date)::INTEGER AS hire_year,
+	   EXTRACT(YEAR FROM e.hire_date)::INTEGER AS hire_year,
 	   ti.title
-  INTO titles_by_hire
+  INTO emp_title_hire_date
   FROM employees AS e
 	   INNER JOIN titles AS ti
 	   ON (e.emp_no = ti.emp_no)
  ORDER BY e.emp_no, ti.to_date DESC;
 
--- Next, use the table created in Deliverable 2 to count the number of employees
--- that are eligible to be mentors and group them by title
-SELECT me.title,
-       COUNT(me.emp_no) AS eligible_mentors
-  INTO mentors_title
-  FROM mentorship_eligibility AS me
- GROUP BY me.title
-
--- Create a similar table with new employees (employees hired in 1999) that
--- would need a mentor grouped by title
-SELECT tbh.title,
-	   COUNT(tbh.emp_no) AS mentors_needed
-  INTO mentors_needed_title
-  FROM titles_by_hire AS tbh
- WHERE tbh.hire_year = 1999
- GROUP BY tbh.title
-
--- Join the table of mentors by title and the mentors needed by title to create
--- a final table showing if the mentoring needs are met
-SELECT mnt.title,
-	   mnt.mentors_needed,
-	   mt.eligible_mentors
+-- Gather the new employees (employees hired in 1999) from emp_title_hire_date
+-- and employees that meet mentorship eligibility from mentorship_eligibility
+-- and group them by title
+SELECT mn.title,
+	   mn.mentors_needed,
+	   em.eligible_mentors
   INTO mentoring_needs
-  FROM mentors_needed_title AS mnt
-	   INNER JOIN mentors_title AS mt
-	   ON (mnt.title = mt.title);
+  FROM (SELECT ethd.title,
+	           COUNT(ethd.emp_no) AS mentors_needed
+          FROM emp_title_hire_date AS ethd
+         WHERE ethd.hire_year = 1999
+         GROUP BY ethd.title) AS mn
+	   INNER JOIN (SELECT me.title,
+                          COUNT(me.emp_no) AS eligible_mentors
+                     FROM mentorship_eligibility AS me
+                    GROUP BY me.title) AS em
+	   ON (mn.title = em.title);
