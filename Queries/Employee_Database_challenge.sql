5a7357f4027cdc1e63fce11acb3ce52d3777dab7
Chris Peterson <cdpeters1@gmail.com>
2022-02-12 18:36:21 -0800

    Add additional queries

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -21,16 +21,16 @@ SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
  ORDER BY rt.emp_no, rt.to_date DESC;
 
 -- Retiring titles table
-SELECT COUNT(ut.title) AS ct,
+SELECT COUNT(ut.title) AS count,
 	   ut.title
   INTO retiring_titles
   FROM unique_titles AS ut
  GROUP BY ut.title
- ORDER BY ct DESC;
+ ORDER BY count DESC;
 
-
+--------------------------------------------------------------------------------
 -- DELIVERABLE 2. Employees Eligible for the Mentorship Program
---
+
 SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	   e.first_name, e.last_name,
 	   e.birth_date,
@@ -46,8 +46,8 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
    AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
  ORDER BY e.emp_no, ti.to_date DESC;
 
-
--- Deliverable 3.
+--------------------------------------------------------------------------------
+-- ADDITIONAL QUERIES OF INTEREST
 
 -- QUESTION 1: How many roles will need to be filled if the employees that meet
 -- retirement eligibility choose to retire?
@@ -59,7 +59,7 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
 	   ti.title,
 	   d.dept_name,
        ti.to_date
-  INTO retiring_title_dept
+  INTO current_retiring
   FROM employees AS e
   	   INNER JOIN titles AS ti
 	   ON (e.emp_no = ti.emp_no)
@@ -71,12 +71,17 @@ SELECT DISTINCT ON (e.emp_no) e.emp_no,
    AND (ti.to_date = '9999-01-01')
  ORDER BY e.emp_no, ti.to_date DESC;
 
--- the number of retirement eligible employees by department
-SELECT rtd.dept_name,
-  	   COUNT(rtd.emp_no) AS ct
-  FROM retiring_title_dept AS rtd
- GROUP BY rtd.dept_name
- ORDER BY ct DESC;
+-- Retirement by department (similar to deliverable 1 retiring_titles)
+SELECT COUNT(cr.emp_no) AS count,
+	   cr.dept_name
+  INTO retiring_dept
+  FROM current_retiring AS cr
+ GROUP BY cr.dept_name
+ ORDER BY count DESC;
+
+-- Total number of positions needing replacing
+SELECT SUM(rd.count) AS total_retiring
+  FROM retiring_dept AS rd;
 
 --------------------------------------------------------------------------------
 -- QUESTION 2: Are there enough qualified, retirement-ready employees to mentor
@@ -122,3 +127,58 @@ SELECT mn.title,
                      FROM mentorship_eligibility AS me
                     GROUP BY me.title) AS em
 	   ON (mn.title = em.title);
+
+--------------------------------------------------------------------------------
+-- the departments of the retiring managers
+SELECT *
+  FROM current_retiring AS cr
+ WHERE cr.title = 'Manager';
+
+ -------------------------------------------------------------------------------
+-- Based on the retiring_dept results, here is a table containing the 3
+-- departments with the most employees retiring and their counts by title
+SELECT DISTINCT ti.title,
+	   COALESCE(dev.development_count, 0) AS development_count,
+	   COALESCE(prod.production_count, 0) AS production_count,
+	   COALESCE(sales.sales_count, 0) AS sales_count
+  INTO top_3_dept_retiring_titles
+  FROM titles AS ti
+       LEFT JOIN (SELECT cr.title,
+		                 COUNT(cr.emp_no) AS development_count
+	                FROM current_retiring AS cr
+	               WHERE cr.dept_name = 'Development'
+	               GROUP BY cr.title) AS dev
+	   ON (ti.title = dev.title)
+	   LEFT JOIN (SELECT cr.title,
+				         COUNT(cr.emp_no) AS production_count
+				    FROM current_retiring AS cr
+		           WHERE cr.dept_name = 'Production'
+		           GROUP BY cr.title) AS prod
+	   ON (ti.title = prod.title)
+	   LEFT JOIN (SELECT cr.title,
+				         COUNT(cr.emp_no) AS sales_count
+				    FROM current_retiring AS cr
+		           WHERE cr.dept_name = 'Sales'
+		           GROUP BY cr.title) AS sales
+	   ON (ti.title = sales.title)
+ ORDER BY ti.title;
+
+--------------------------------------------------------------------------------
+-- -- (optional) table showing percent retiring by department
+-- SELECT ce.dept_name,
+-- 	     ce.current_employees,
+-- 	     re.retiring_employees,
+-- 	     TO_CHAR(100 * (re.retiring_employees::FLOAT / ce.current_employees::FLOAT), 'fm00D0%') AS percent_retiring
+--   FROM (SELECT d.dept_name,
+-- 	             COUNT(de.emp_no) as current_employees
+--           FROM dept_emp AS de
+--                INNER JOIN departments AS d
+--                ON (de.dept_no = d.dept_no)
+--          WHERE de.to_date = '9999-01-01'
+--          GROUP BY d.dept_name) AS ce
+--        INNER JOIN (SELECT cr.dept_name,
+-- 	                        COUNT(cr.emp_no) AS retiring_employees
+--                      FROM current_retiring AS cr
+--                     GROUP BY cr.dept_name) AS re
+-- 	     ON (ce.dept_name = re.dept_name)
+--  ORDER BY re.retiring_employees DESC;
